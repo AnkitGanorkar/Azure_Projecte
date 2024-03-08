a3d6018f7f26905c92261042f119e032310439ac
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-03-08 23:59:42 +0530

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -6,7 +6,7 @@ $sname = "databasetargetlifter.mysql.database.azure.com";
 $uname = "databasetargetlifter";
 $password = "xN]3PxKgeT9,^NuxN]3PxKgeT9,^Nu";
 
-$db_name = "targetlifter00";
+$db_name = "databasetargetlifter";
 
 $conn = new mysqli($sname, $uname, $password, $db_name);
 
